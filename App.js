6159a6dc7e9d0d5031e3bf7edce175e41b7770d8
sangwoo98 <sangwoo98.kang@gmail.com>
2021-04-11 15:26:42 +0900

    init commit

--- App.js
+++ App.js
@@ -1,16 +1,41 @@
-import * as React from "react";
-import { View, Text } from "react-native";
+import React, {Component} from 'react';
+import {StyleSheet, Text, View, Image, Button, Alert, TextInput} from 'react-native';
 
-export default function App() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Universal React with Expo</Text>
-    </View>
-  );
-}
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <View style={styles.container}>
+          <Image
+            style={{height:'80%',width:'100%'}}
+            source={require('./pngwing.com.png')}/>
+          <Text style={styles.title}>
+            아이디/비밀번호 저장기
+          </Text>
+          <Button
+            title="네이버"
+            color="#f194ff"
+            onPress={() => Alert.alert('abcd<br>1234')}
+          />
+      </View>
+    );
+  }
+} 
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 10,
+    backgroundColor: 'white',
+  },
+  title: {
+    textAlign: 'center',
+    marginVertical: 8,
+    color: '#e0400f',
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+  }
+});
